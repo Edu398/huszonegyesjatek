48faf7f303cd732ccd097d37b329e9f3e3a1a9d1
Edu398 <fejes.edu@gmail.com>
2023-02-03 11:22:58 +0100

    masodikprobalkozas

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 """megoldás"""
 
 
-def eredmeny():
+def eredmeny(jatekos, gep):
+    szoveg=""
+    if jatekos > 21 and gep > 21:
+        szoveg="döntetlen"
+    elif jatekos > 21:
+        szoveg="vesztettél"
+    elif gep > 21:
+        szoveg ="nyertél"
+    return szoveg
+
 
     """tesztesetek"""
